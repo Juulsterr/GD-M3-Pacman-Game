4c097c10656de0e4f4a1f6e8998e9b90d170adf9
Julian <40456@ma-web.nl>
2026-03-03 13:05:15 +0100

    nog-een-les

--- Assets/Scripts/Speler.cs
+++ Assets/Scripts/Speler.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
 
-public class Speler : MonoBehaviour
+public class PacmanMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+
+    private Vector2 direction = Vector2.right; // start richting
+    private Vector3 originalScale;
+
     void Start()
     {
-        
+        originalScale = transform.localScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W)) direction = Vector2.up;
+        if (Input.GetKeyDown(KeyCode.S)) direction = Vector2.down;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction = Vector2.left;
+            transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction = Vector2.right;
+            transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(direction * speed * Time.fixedDeltaTime);
     }
 }
