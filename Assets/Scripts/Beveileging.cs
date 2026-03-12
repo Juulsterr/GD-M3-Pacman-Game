27053079d2aa9ef9d2e1ca264dbb35478b30b8b7
Julian <40456@ma-web.nl>
2026-03-12 12:18:42 +0100

    niew

--- Assets/Scripts/Beveileging.cs
+++ Assets/Scripts/Beveileging.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class Beveiliging : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 2f;
+
+    private Vector2 direction = Vector2.right; // start richting
+    private Vector3 originalScale;
+
     void Start()
     {
-        
+        originalScale = transform.localScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow)) direction = Vector2.up;
+        if (Input.GetKeyDown(KeyCode.DownArrow)) direction = Vector2.down;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
+            transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+            transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(direction * speed * Time.fixedDeltaTime);
     }
 }
--- Assets/Scripts/Speler.cs
+++ Assets/Scripts/Speler.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PacmanMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public int score = 0;
+    public int health = 100;
 
     private Vector2 direction = Vector2.right; // start richting
     private Vector3 originalScale;
@@ -33,5 +35,21 @@ public class PacmanMovement : MonoBehaviour
     void FixedUpdate()
     {
         transform.Translate(direction * speed * Time.fixedDeltaTime);
+    }
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+         if (collision.gameObject.tag == "Enemy")
+        {
+            health -= 100;
+
+            if (health <= 0)
+            {
+                Die();
+            }
+        }
+    }
+    private void Die()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name); // Reload the current scene
     }
 }
