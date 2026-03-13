9faa6fea3175721c990a41f94d78f41e88a1f0b1
Julian <40456@ma-web.nl>
2026-03-13 15:18:34 +0100

    fix

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -4,9 +4,14 @@ using TMPro;
 public class ScoreUI : MonoBehaviour
 {
     public TMP_Text scoreText;
+    void Start()
+    {
+        ScoreManager.score = 0; // Reset de score bij het starten van het spel
+    }
 
     void Update()
     {
         scoreText.text = "Score: " + ScoreManager.score;
+        
     }
 }
