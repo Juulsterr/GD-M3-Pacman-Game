1498341d7a4d524494b38efc2d514ec6e5e96c81
Alysha-Braafhart <236565157+Alysha-Braafhart@users.noreply.github.com>
2026-03-17 09:27:55 +0100

    The door

--- Assets/Scripts/GoldenRings.cs
+++ Assets/Scripts/GoldenRings.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class GoldenRings : MonoBehaviour
 {
+   public int goldenRings = 0;
+    public static int goldenRingsRequired = 4;
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-        
+            
             PacmanMovement player = collision.gameObject.GetComponent<PacmanMovement>();
-            player.goldenRings += 1;
+            PacmanMovement.goldenRings += 1;
             ScoreManager.AddScore(10);
             Destroy(gameObject);
+            goldenRingsRequired -= 1;
         }
     }
 }
