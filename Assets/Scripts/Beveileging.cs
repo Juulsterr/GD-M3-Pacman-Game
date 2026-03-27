a699f24935042c46dafc725a777095d60a2da18f
Alysha-Braafhart <236565157+Alysha-Braafhart@users.noreply.github.com>
2026-03-27 14:08:38 +0100

    Pls work

--- Assets/Scripts/Beveileging.cs
+++ Assets/Scripts/Beveileging.cs
@@ -14,22 +14,35 @@ public class Beveiliging : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) direction = Vector2.up;
-        if (Input.GetKeyDown(KeyCode.DownArrow)) direction = Vector2.down;
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+         if (Input.GetAxis("VerticalController2") > 0.99f) direction = Vector3.up;
+        if (Input.GetAxis("VerticalController2") < -0.99f) direction = Vector3.down;
+        if (Input.GetAxis("HorizontalController2") < -0.99f)
         {
-            direction = Vector2.left;
-            transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
+            direction = Vector3.left;
+            transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z); 
         }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+         
+        if (Input.GetAxis("HorizontalController2") > 0.99f)
         {
-            direction = Vector2.right;
-            transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
-        }
+            direction = Vector3.right;
+            transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);        
+        } 
+        // if (Input.GetKeyDown(KeyCode.UpArrow)) direction = Vector2.up;
+        // if (Input.GetKeyDown(KeyCode.DownArrow)) direction = Vector2.down;
+
+        // if (Input.GetKeyDown(KeyCode.LeftArrow))
+        // {
+        //     direction = Vector2.left;
+        //     transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
+        // }
+
+        // if (Input.GetKeyDown(KeyCode.RightArrow))
+        // {
+        //     direction = Vector2.right;
+        //     transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
+        // }
     }
-  // gaylord
+
 
     void FixedUpdate()
     {
